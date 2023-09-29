95206cd0fdd009af01d8cd74fd3ea9926a9da57e
luigichelli <12992112+luigichelli@users.noreply.github.com>
2023-09-29 18:53:37 +0200

    svg structure like figma

--- hex_to_svg.py
+++ hex_to_svg.py
@@ -65,8 +65,8 @@ if st.button("Run"):
         
         # Create a new SVG with a simplified structure
         svg_content = f'''
-        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-            <rect width="64" height="64" fill="{hex_value}" />
+        <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="64" height="64" fill="{hex_value}"/>
         </svg>
         '''
         
